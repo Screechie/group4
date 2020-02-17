141a757e23917238fda0c4fb8672087466eef3be
Screechie <5440543+Screechie@users.noreply.github.com>
2020-02-17 17:32:00 -0400

    Added lightbox code

--- gallery_assets/gallery_js/gallery-nature.js
+++ gallery_assets/gallery_js/gallery-nature.js
@@ -1,3 +1,21 @@
-let modal = () => {
-  
-}
+let lightbox = document.createElement('div');
+
+lightbox.id = 'lightbox';
+
+// Append lightbox to body of html document
+document.body.appendChild(lightbox);
+
+// collect all nature images
+const lightboxImages = document.getElementsByClassName('nature-image');
+
+//Lopp t
+lightboxImages.forEaach( (img) => {
+  img.addEventListener('click', (event) => {
+    lightbox.classList.add('active');
+    const image =document.createElement('img');
+    image.src = img.src;
+    lightbox.appendChild(image);
+  });
+});
+
+console.log(lightboxImages);
